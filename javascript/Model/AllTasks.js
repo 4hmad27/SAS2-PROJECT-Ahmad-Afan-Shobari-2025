b03b799d96420f0eeb916e1111ee6752e42477f8
Ahmad Afan <arte.ra272024@gmail.com>
2025-05-23 13:30:43 +0700

    create: method edit and problem solving

--- javascript/Model/AllTasks.js
+++ javascript/Model/AllTasks.js
@@ -1,83 +1,76 @@
 class AllTasks {
-    constructor(
-      title,
-      description,
-      status,
-      dueDate
-    ) {
-        this.title = title
-        this.description = description
-        this.status = status
-        this.dueDate = dueDate
-    }
-    
-    async getAllTasks(){
-        try {
-            return await fetch("https://68258f1d0f0188d7e72d6675.mockapi.io/api/todos", {
-                method: "GET",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            ).then((response) => response.json());
-        } catch (error) {
-            console.log("error while getting data", error)
+  async getAllTasks() {
+    try {
+      return await fetch(
+        "https://68258f1d0f0188d7e72d6675.mockapi.io/api/todos",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      ).then((response) => response.json());
+    } catch (error) {
+      console.log("error while getting data", error);
     }
-  
-    async deleteAllTasks(id) {
-      try {
-        const isConfirm = confirm(
-            `apakah anda yakin ingin menghapus data ini ${id}?`
-          );
-
-          if (isConfirm) {
-            const deleteUser = await fetch(
-              `https://68258f1d0f0188d7e72d6675.mockapi.io/api/todos/${id}`,
-              {
-                method: "DELETE",
-              }
-            );
-            // console.log(deleteUser);
-            if (deleteUser.ok) {
-              alert(`data user dengan id ${id} berhasil dihapus`);
-            } else {
-              alert(
-                `data user dengan id ${id} gagal dihapus :${deleteUser.status}`
-              );
-            }
-          }
-      } catch (error) {
-        console.error("error while delete data:", error);
-      }
-    }
-  
-    editAllTasks(id, newname){
-      try {
-        if(!id) {
-          throw new Error("invalid ID. ID are required");
-        }
-    
-        if(!newname) {
-          throw new Error("invalid name. name are required");
-        }
-  
-        let semuaDataJenisPelajaran = this.getJenisPenilaianfromLocalStorage()
-        
-        console.log("ini yang ada di edit class",semuaDataJenisPelajaran)
-        let editDataJenisPelajaran = semuaDataJenisPelajaran.filter((data) => data.jenis_penilaian_id === id)
-  
-  
-        editDataJenisPelajaran[0].nama_jenis_penilaian = newname
-        
-        this.saveJenisPenilaianToLocalStorage(semuaDataJenisPelajaran)
-        alert("you succes for update kategori");
-      } catch (error) {
-        console.error("error while editing data:", error);
-      }
-    }
-  
-    
   }
-  export { AllTasks };
-  
+
+  async deleteAllTasks(id) {
+    try {
+      const isConfirm = confirm(
+        `apakah anda yakin ingin menghapus data ini ${id}?`
+      );
+
+      if (isConfirm) {
+        const deleteUser = await fetch(
+          `https://68258f1d0f0188d7e72d6675.mockapi.io/api/todos/${id}`,
+          {
+            method: "DELETE",
+          }
+        );
+        // console.log(deleteUser);
+        if (deleteUser.ok) {
+          alert(`data user dengan id ${id} berhasil dihapus`);
+        } else {
+          alert(
+            `data user dengan id ${id} gagal dihapus :${deleteUser.status}`
+          );
+        }
+      }
+    } catch (error) {
+      console.error("error while delete data:", error);
+    }
+  }
+
+  async editAllTasks(id, title, description, status, date) {
+    try {
+        console.log(id, title, description, status, date);
+      const editData = await fetch(
+        `https://68258f1d0f0188d7e72d6675.mockapi.io/api/todos/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "title": title,
+            "description": description,
+            "is_completed": status,
+            "dueDate": date,
+          }),
+        }
+      );
+      console.log(editData);
+    //   if (!response.ok) {
+    //     throw new Error(`HTTP error! status: ${response.status}`);
+    //   }
+
+      const data = await editData.json();
+      console.log(data);
+      alert(`data user dengan id ${id} berhasil diupdate`);
+    } catch (error) {
+      console.error("error while editing data:", error);
+    }
+  }
+}
+export { AllTasks };
